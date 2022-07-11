823d00004ee178aa24c5efcf5e20332921243c99
petehodnefield <98546095+petehodnefield@users.noreply.github.com>
2022-07-11 12:28:29 -0500

    About tests working

---
+++ src/components/About/__tests__/index.test.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {render, cleanup} from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import About from '..'
+
+afterEach(cleanup)
+describe('About component', () => {
+    // renders About test
+    // First
+    it('renders', () => {
+        render(<About />)
+    })
+    // Second
+    it('matches snapshot DOM node structure', () => {
+        // render About
+        const {asFragment} = render(<About />)
+        expect(asFragment()).toMatchSnapshot()
+    })
+})
